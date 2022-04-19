43710c5f8e12cffa5f5c93b8ad03f9411fcb7b20
joaovictorrb <joaov.rbrum@gmail.com>
2022-04-18 23:12:56 -0300

    simple crud, for now

--- blog/app/Controllers/Http/ArticlesController.ts
+++ blog/app/Controllers/Http/ArticlesController.ts
@@ -11,11 +11,32 @@ import Article from 'App/Models/Article'
  * ? => updateCommentary
  */
 export default class ArticlesController {
+  public async getArticles(param, {request, response}: HttpContextContract) {
+    const article = await Article.findOrFail(param.articleId)
+    if(article) {
+      return article
+    }
+    const allArticles = await Article.all()
+    return allArticles
+  }
+
 	public async newArticle({ request, response }: HttpContextContract) {
     const article_data = request.only[('subject', 'content')]
-
     await Article.create(article_data)
-
     return response.created
   }
+
+  public async updateArticle(param, { request, response }: HttpContextContract) {
+    const article_data = request.only[('subject', 'content')]
+    const article = await Article.findOrFail(param.articleId)
+    await article.merge(article_data).save()
+    return response.accepted
+  }
+
+  public async delete(param, {response}: HttpContextContract) {
+    const article = await Article.findOrFail(param.articleId)
+    await article.delete()
+    return response.accepted
+  }
+
 }
